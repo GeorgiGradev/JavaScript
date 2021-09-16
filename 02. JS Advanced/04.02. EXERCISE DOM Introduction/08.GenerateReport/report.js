46fa5c22fb596d88e2670a2d97eb81a7aa389df4
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-09-16 11:08:13 +0300

    Update report.js

--- 02. JS Advanced/04.02. EXERCISE DOM Introduction/08.GenerateReport/report.js
+++ 02. JS Advanced/04.02. EXERCISE DOM Introduction/08.GenerateReport/report.js
@@ -1,7 +1,7 @@
 
 function generateReport() {
     const output = document.getElementById('output');
-    const checkbuttons = Array.from(document.querySelectorAll('thead th input')); // TODO
+    const checkbuttons = Array.from(document.querySelectorAll('thead th input'));
 
     let indexes = [];
     checkbuttons.forEach((b, i) => {
@@ -12,13 +12,13 @@ function generateReport() {
 
     let resultArray = [];
     const tableHead = document.querySelectorAll('thead th');
-    const tableRows = Array.from(document.querySelectorAll('tbody tr')); // TODO
+    const tableRows = Array.from(document.querySelectorAll('tbody tr'));
 
     for (const row of tableRows) {
         let obj = {};
         for (const index of indexes) {
             let currentRow = row.children[index].textContent;
-            let currentName = tableHead[index].textContent.toLowerCase().trim(); // TODO
+            let currentName = tableHead[index].textContent.toLowerCase().trim();
             obj[currentName] = currentRow;
         }
         resultArray.push(obj);

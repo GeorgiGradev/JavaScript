9e7e9f6c18991d70a75b1645a89818b6ee64a839
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-10-04 20:06:44 +0300

    Create app.js

---
+++ 03. JS Applications/02.02. EXERCISE Asynchronous Programming/02.Bus-Schedule/app.js
@@ -0,0 +1,44 @@
+function solve() {
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    const banner = document.querySelector('#info span');
+
+    let stop = {
+        next: 'depot',
+        nme: undefined,
+    };
+
+    async function depart() {
+        // request about upcoming stop
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        stop = data;
+
+
+        // update banner with stop name
+        banner.textContent = `Next stop ${stop.name}`;
+
+        //activate other button
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
+
+    }
+
+    function arrive() {
+        // update banner to show ARRIVAL
+        banner.textContent = `Arriving at ${stop.name}`;
+
+        //activate other button
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
+    }
+
+    return {
+        depart,
+        arrive
+    };
+}
+
+let result = solve();

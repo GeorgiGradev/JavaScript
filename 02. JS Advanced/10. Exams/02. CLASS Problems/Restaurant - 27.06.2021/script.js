8f51ac80ed7382a02ae0cf2f321b23c491a9a1a7
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-10-20 15:39:08 +0300

    Create script.js

---
+++ 02. JS Advanced/10. Exams/02. CLASS Problems/Restaurant - 27.06.2021/script.js
@@ -0,0 +1,87 @@
+class Restaurant {
+    constructor(budget) {
+        this.budgetMoney  = budget;
+        this.menu = {};
+        this.stockProducts = {};
+        this.history = [];
+    }
+
+    loadProducts(products) {
+        for (const product of products) {
+            let [productName, productQuantity, productPrice] = product.split(' ');
+            productQuantity = Number(productQuantity);
+            productPrice = Number(productPrice);
+
+            if (this.budgetMoney >= productPrice) {
+                if (this.stockProducts.hasOwnProperty(productName)) {
+                    this.stockProducts[productName] += productQuantity;
+                } else {
+                    this.stockProducts[productName] = productQuantity;
+                }
+                this.budgetMoney -= productPrice;
+                this.history.push(`Successfully loaded ${productQuantity} ${productName}`);
+            } else {
+                this.history.push(`There was not enough money to load ${productQuantity} ${productName}`);
+            }
+        }
+        return this.history.join('\n');
+    }
+
+    addToMenu(meal, neededProducts, price) {
+        if (this.menu.hasOwnProperty(meal)) {
+            return `The ${meal} is already in the our menu, try something different.`;
+        }
+        this.menu[meal] = {
+            neededProducts,
+            price
+        };
+        if (Object.keys(this.menu).length == 1) {
+            return `Great idea! Now with the ${meal} we have 1 meal in the menu, other ideas?`;
+        } else {
+            return `Great idea! Now with the ${meal} we have ${Object.keys(this.menu).length} meals in the menu, other ideas?`;
+        }
+    }
+
+    showTheMenu() {
+        if (Object.keys(this.menu).length == 0) {
+            return `Our menu is not ready yet, please come later...`;
+        }
+
+        const output = [];
+        for (const [meal, object] of Object.entries(this.menu)) {
+            output.push(`${meal} - $ ${object.price}`);
+        }
+
+        return output.join('\n');
+    }
+
+    makeTheOrder(mealName) {
+        if (this.menu.hasOwnProperty(mealName) == false) {
+            return `There is not ${mealName} yet in our menu, do you want to order something else?`;
+        }
+
+        const meal = this.menu[mealName];
+
+        for (const element of meal.neededProducts) {
+            const tokens = element.split(' ');
+            const singleProduct = tokens[0];
+            const quantity = tokens[1];
+
+            if (this.stockProducts.hasOwnProperty(singleProduct) == false || this.stockProducts[singleProduct] < quantity) {
+                return `For the time being, we cannot complete your order (${mealName}), we are very sorry...`;
+            }
+        }
+
+        for (const element of meal.neededProducts) {
+            const tokens = element.split(' ');
+            const singleProduct = tokens[0];
+            const quantity = tokens[1];
+
+            this.stockProducts[singleProduct] -= quantity;
+            this.budgetMoney += meal.price;
+
+        }
+
+        return `Your order (${mealName}) will be completed in the next 30 minutes and will cost you ${meal.price}.`;
+    }
+}

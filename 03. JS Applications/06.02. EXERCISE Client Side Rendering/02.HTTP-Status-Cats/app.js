e941dbcf0ed5e8c196e37801a743450428e2e120
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-10-28 11:15:10 +0300

    Update app.js

--- 03. JS Applications/06.02. EXERCISE Client Side Rendering/02.HTTP-Status-Cats/app.js
+++ 03. JS Applications/06.02. EXERCISE Client Side Rendering/02.HTTP-Status-Cats/app.js
@@ -6,7 +6,7 @@ const template = (cat) => html`
         <li>
             <img src="./images/${cat.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
             <div class="info">
-                <button class="showBtn">Show status code</button>
+                <button class="showBtn">${cat.info ? 'Show' : 'Hide'}</button>
                 <div class="status" style=${styleMap(cat.info ?  {display: 'none'} :{} )} id=${cat.id}>
                     <h4>Status Code: ${cat.statusCode}</h4>
                     <p>${cat.statusMessage}</p>

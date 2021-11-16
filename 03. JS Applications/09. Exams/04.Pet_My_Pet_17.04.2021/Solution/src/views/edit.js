5e151e6da9b3abf2dc70a649212c2bbe59648c11
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-11-16 15:54:58 +0200

    Create edit.js

---
+++ 03. JS Applications/09. Exams/04.Pet_My_Pet_17.04.2021/Solution/src/views/edit.js
@@ -0,0 +1,68 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { editPet, getPetById } from '../api/data.js';
+
+const editTemplate = (pet, onSubmit) => html`
+<section id="edit-page" class="edit">
+    <form @submit=${onSubmit} id="edit-form" action="#" method="">
+        <fieldset>
+            <legend>Edit my Pet</legend>
+            <p class="field">
+                <label for="name"></label>
+                <span class="input">
+                    <input type="text" name="name" id="name" value=${pet.name}>
+                </span>
+            </p>
+            <p class="field">
+                <label for="description">Description</label>
+                <span class="input">
+                    <textarea name="description"
+                        id="description">${pet.description}</textarea>
+                </span>
+            </p>
+            <p class="field">
+                <label for="image">Image</label>
+                <span class="input">
+                    <input type="text" name="imageUrl" id="image" value=${pet.imageUrl}>
+                </span>
+            </p>
+            <p class="field">
+                <label for="type">Type</label>
+                <span class="input">
+                    <select id="type" name="type" .value=${pet.type.toLowerCase()}>
+                        <option value="cat">Cat</option>
+                        <option value="dog">Dog</option>
+                        <option value="parrot">Parrot</option>
+                        <option value="reptile">Reptile</option>
+                        <option value="other">Other</option>
+                    </select>
+                </span>
+            </p>
+            <input class="button submit" type="submit" value="Save">
+        </fieldset>
+    </form>
+</section>`;
+
+export async function editPage(ctx) {
+    const petId = ctx.params.id;
+    const pet = await getPetById(petId);
+    ctx.render(editTemplate(pet, onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const editedPet = {
+            name: formData.get('name').trim(),
+            description: formData.get('description').trim(),
+            imageUrl: formData.get('imageUrl').trim(),
+            type: formData.get('type').trim(),
+        };
+
+        if (Object.values(editedPet).some(x => !x)) {
+            return alert('All fields are required!');
+        }
+
+        await editPet(petId, editedPet);
+        event.target.reset();
+        ctx.page.redirect(`/details/` + petId);
+    }
+}

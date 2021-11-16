c2e3ef74a38662ee4f18b6b959c02b6ab7d8438b
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-11-16 15:55:07 +0200

    Create auth.js

---
+++ 03. JS Applications/09. Exams/04.Pet_My_Pet_17.04.2021/Solution/src/views/auth.js
@@ -0,0 +1,94 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { login, register } from '../api/data.js';
+
+
+const loginTemplate = (onSubmit) => html`
+<section id="login-page" class="login">
+    <form @submit=${onSubmit} id="login-form" action="" method="">
+        <fieldset>
+            <legend>Login Form</legend>
+            <p class="field">
+                <label for="email">Email</label>
+                <span class="input">
+                    <input type="text" name="email" id="email" placeholder="Email">
+                </span>
+            </p>
+            <p class="field">
+                <label for="password">Password</label>
+                <span class="input">
+                    <input type="password" name="password" id="password" placeholder="Password">
+                </span>
+            </p>
+            <input class="button submit" type="submit" value="Login">
+        </fieldset>
+    </form>
+</section>`;
+
+export async function loginPage(ctx) {
+    ctx.render(loginTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+
+        await login(email, password);
+
+        event.target.reset();
+        ctx.setUserNav();
+        ctx.page.redirect('/');
+    }
+}
+
+const registerTemplate = (onSubmit) => html`
+<section id="register-page" class="register">
+    <form @submit=${onSubmit} id="register-form" action="" method="">
+        <fieldset>
+            <legend>Register Form</legend>
+            <p class="field">
+                <label for="email">Email</label>
+                <span class="input">
+                    <input type="text" name="email" id="email" placeholder="Email">
+                </span>
+            </p>
+            <p class="field">
+                <label for="password">Password</label>
+                <span class="input">
+                    <input type="password" name="password" id="password" placeholder="Password">
+                </span>
+            </p>
+            <p class="field">
+                <label for="repeat-pass">Repeat Password</label>
+                <span class="input">
+                    <input type="password" name="confirm-pass" id="repeat-pass" placeholder="Repeat Password">
+                </span>
+            </p>
+            <input class="button submit" type="submit" value="Register">
+        </fieldset>
+    </form>
+</section>`;
+
+export async function registerPage(ctx) {
+    ctx.render(registerTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+        const repeatPass = formData.get('confirm-pass').trim();
+
+        if (!email || !password || !repeatPass) {
+            return alert('All fields are requirerd!');
+        }
+        if (password != repeatPass) {
+            return alert('Password don\'t match!');
+        }
+        await register(email, password);
+
+        event.target.reset();
+        ctx.setUserNav();
+        ctx.page.redirect('/');
+    }
+}

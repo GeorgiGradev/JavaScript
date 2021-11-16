83d92900f7360d3505327afa9b181eceaadc81c2
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-11-16 15:55:04 +0200

    Create pet.js

---
+++ 03. JS Applications/09. Exams/04.Pet_My_Pet_17.04.2021/Solution/src/views/common/pet.js
@@ -0,0 +1,9 @@
+import { html } from '../../../node_modules/lit-html/lit-html.js';
+
+export const petTemplate = (pet) => html`
+<li class="otherPet">
+    <h3>Name: ${pet.name}</h3>
+    <p>Type: ${pet.type}</p>
+    <p class="img"><img src=${pet.imageUrl}></p>
+    <a class="button" href="/details/${pet._id}">Details</a>
+</li>`;

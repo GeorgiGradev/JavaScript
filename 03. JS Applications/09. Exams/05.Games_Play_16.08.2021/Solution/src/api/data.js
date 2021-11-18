2c77e3b562ba06127e4bdca747370f0db779292a
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-11-18 18:28:56 +0200

    Create data.js

---
+++ 03. JS Applications/09. Exams/05.Games_Play_16.08.2021/Solution/src/api/data.js
@@ -0,0 +1,49 @@
+import * as api from './api.js';
+
+const host = 'http://localhost:3030';
+api.settings.host = host;
+
+export const login = api.login;
+export const register = api.register;
+export const logout = api.logout;
+
+// Application specific requests
+// get all
+export async function getAllGames() {
+    return await api.get(host + `/data/games?sortBy=_createdOn%20desc`);
+}
+
+//Home page
+export async function getRecentGames(){
+    return await api.get(host + `/data/games?sortBy=_createdOn%20desc&distinct=category`);
+}
+
+// Create
+export async function createGame(game) {
+    return await api.post(host + `/data/games`, game);
+}
+
+// Get by id
+export async function getGameById(gameId) {
+    return await api.get(host + `/data/games/` + gameId);
+}
+
+// Edit
+export async function updateGame(gameId, updatedGame) {
+    return await api.put(host + '/data/games/' + gameId, updatedGame);
+}
+
+// Delete
+export async function deleteGame(id) {
+    return await api.del(host + '/data/games/' + id);
+}
+
+//Get all comments
+export async function getComments(gameId){
+    return await api.get(host + `/data/comments?where=gameId%3D%22${gameId}%22`);
+}
+
+//Create comment
+export async function createComment(comment){
+    return await api.post(host + `/data/comments`, comment);
+}

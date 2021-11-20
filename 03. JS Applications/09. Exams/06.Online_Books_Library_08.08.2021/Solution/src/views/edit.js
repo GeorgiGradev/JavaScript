3815b1d7dad7cc655eeca6cf0e639614014ae757
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-11-20 12:47:48 +0200

    Create edit.js

---
+++ 03. JS Applications/09. Exams/06.Online_Books_Library_08.08.2021/Solution/src/views/edit.js
@@ -0,0 +1,68 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { getBookById, updateBook } from '../api/data.js';
+
+const editTemplate = (book, onSubmit) => html`
+<section id="edit-page" class="edit">
+    <form @submit=${onSubmit} id="edit-form" action="#" method="">
+        <fieldset>
+            <legend>Edit my Book</legend>
+            <p class="field">
+                <label for="title">Title</label>
+                <span class="input">
+                    <input type="text" name="title" id="title" value=${book.title}>
+                </span>
+            </p>
+            <p class="field">
+                <label for="description">Description</label>
+                <span class="input">
+                    <textarea name="description"
+                        id="description">${book.description}</textarea>
+                </span>
+            </p>
+            <p class="field">
+                <label for="image">Image</label>
+                <span class="input">
+                    <input type="text" name="imageUrl" id="image" value=${book.imageUrl}>
+                </span>
+            </p>
+            <p class="field">
+                <label for="type">Type</label>
+                <span class="input">
+                    <select id="type" name="type" .value=${book.type}>
+                        <option value="Fiction">Fiction</option>
+                        <option value="Romance">Romance</option>
+                        <option value="Mistery">Mistery</option>
+                        <option value="Classic">Clasic</option>
+                        <option value="Other">Other</option>
+                    </select>
+                </span>
+            </p>
+            <input class="button submit" type="submit" value="Save">
+        </fieldset>
+    </form>
+</section>`;
+
+export async function editPage(ctx) {
+    const bookId = ctx.params.id;
+    const book = await getBookById(bookId);
+    ctx.render(editTemplate(book, onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const updatedBook = {
+            title: formData.get('title').trim(),
+            type: formData.get('type').trim(),
+            imageUrl: formData.get('imageUrl').trim(),
+            description: formData.get('description').trim(),
+        };
+
+        if (Object.values(updatedBook).some(x => !x)) {
+            return alert('All fields are required!');
+        }
+
+        await updateBook(bookId, updatedBook);
+        event.target.reset();
+        ctx.page.redirect('/details/' + bookId);
+    }
+}

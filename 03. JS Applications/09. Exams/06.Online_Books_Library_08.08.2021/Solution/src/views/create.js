8877dcc8977836b2cf1859b989f2c3fb284dedaa
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-11-20 12:48:03 +0200

    Create create.js

---
+++ 03. JS Applications/09. Exams/06.Online_Books_Library_08.08.2021/Solution/src/views/create.js
@@ -0,0 +1,65 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { createBook } from '../api/data.js';
+
+const createTemplate = (onSubmit) => html`
+<section id="create-page" class="create">
+    <form @submit=${onSubmit} id="create-form" action="" method="">
+        <fieldset>
+            <legend>Add new Book</legend>
+            <p class="field">
+                <label for="title">Title</label>
+                <span class="input">
+                    <input type="text" name="title" id="title" placeholder="Title">
+                </span>
+            </p>
+            <p class="field">
+                <label for="description">Description</label>
+                <span class="input">
+                    <textarea name="description" id="description" placeholder="Description"></textarea>
+                </span>
+            </p>
+            <p class="field">
+                <label for="image">Image</label>
+                <span class="input">
+                    <input type="text" name="imageUrl" id="image" placeholder="Image">
+                </span>
+            </p>
+            <p class="field">
+                <label for="type">Type</label>
+                <span class="input">
+                    <select id="type" name="type">
+                        <option value="Fiction">Fiction</option>
+                        <option value="Romance">Romance</option>
+                        <option value="Mistery">Mistery</option>
+                        <option value="Classic">Clasic</option>
+                        <option value="Other">Other</option>
+                    </select>
+                </span>
+            </p>
+            <input class="button submit" type="submit" value="Add Book">
+        </fieldset>
+    </form>
+</section>`;
+
+export async function createPage(ctx) {
+    ctx.render(createTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const book = {
+            title: formData.get('title').trim(),
+            type: formData.get('type').trim(),
+            imageUrl: formData.get('imageUrl').trim(),
+            description: formData.get('description').trim(),
+        };
+
+        if (Object.values(book).some(x => !x)) {
+            return alert('All fields are required!');
+        }
+
+        await createBook(book);
+        event.target.reset();
+        ctx.page.redirect('/');
+    }
+}

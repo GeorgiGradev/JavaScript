541ba8ffab2d08f5fe63811f8ae2575d0e084f55
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-11-22 13:56:13 +0200

    Login and register functionality done

---
+++ 03. JS Applications/09. Exams/Car_Tube_sandbox/src/api/data.js
@@ -0,0 +1,44 @@
+import * as api from './api.js';
+
+const host = 'http://localhost:3030';
+api.settings.host = host;
+
+export const login = api.login;
+export const register = api.register;
+export const logout = api.logout;
+
+// Application specific requests
+// get all
+export async function getAllCars() {
+    return await api.get(host + `/data/cars?sortBy=_createdOn%20desc`);
+}
+
+// Create
+export async function createCar(car) {
+    return await api.post(host + `/data/cars`, car);
+}
+
+// Get by id
+export async function getCarById(carId) {
+    return await api.get(host + `/data/cars/` + carId);
+}
+
+// Edit
+export async function updateCar(carId, updatedCar) {
+    return await api.put(host + `/data/cars/` + carId, updatedCar);
+}
+
+// Delete
+export async function deleteCar(carId) {
+    return await api.del(host + `/data/cars/` + carId);
+}
+
+// Get my products
+export async function getMyCars(userId) {
+    return await api.get(host + `/data/cars?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`);
+}
+
+// Search
+export async function search(query) {
+    return await api.get(host + `/data/cars?where=year%3D${query}`);
+}

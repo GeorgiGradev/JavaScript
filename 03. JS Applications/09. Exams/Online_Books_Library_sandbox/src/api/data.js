cdd590a390caf75c83f80f012a7c19f982904017
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-11-23 12:49:52 +0200

    add navigation bar functionality

--- 03. JS Applications/09. Exams/Online_Books_Library_sandbox/src/api/data.js
+++ 03. JS Applications/09. Exams/Online_Books_Library_sandbox/src/api/data.js
@@ -8,79 +8,47 @@ export const register = api.register;
 export const logout = api.logout;
 
 // Application specific requests
-// get all
-export async function getAllGames() {
-    return await api.get(host + `/data/games?sortBy=_createdOn%20desc`);
-}
-
-// Get recent
-export async function getRecentGames(){
-    return await api.get(host + `/data/games?sortBy=_createdOn%20desc&distinct=category`);
-}
-
 // Create
-export async function createGame(game) {
-    return await api.post(host + `/data/games`, game);
+export async function createBook(book) {
+    return await api.post(host + `/data/books`, book);
+}
+
+// get all
+export async function getAllBooks() {
+    return await api.get(host + `/data/books?sortBy=_createdOn%20desc`);
 }
 
 // Get by id
-export async function getGameById(gameId) {
-    return await api.get(host + `/data/games/` + gameId);
+export async function getBookById(bookId) {
+    return await api.get(host + `/data/books/` + bookId);
 }
 
 // Edit
-export async function updateGame(gameId, updatedGame) {
-    return await api.put(host + `/data/games/` + gameId, updatedGame);
+export async function editBook(bookId, updatedBook) {
+    return await api.put(host + `/data/books/` + bookId, updatedBook);
 }
 
 // Delete
-export async function deleteGame(id) {
-    return await api.del(host + `/data/games/` + id);
+export async function deleteBook(id) {
+    return await api.del(host + `/data/books/` + id);
 }
 
 // Get my products
-export async function getMyListings(userId) {
-    return await api.get(host + `/data/cars?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`);
+export async function getMyBooks(userId) {
+    return await api.get(host + `/data/books?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`);
 }
 
-// === BONUS ===
-
-//Get all comments (Comments)
-export async function getComments(gameId){
-    return await api.get(host + `/data/comments?where=gameId%3D%22${gameId}%22`);
+//like 
+export async function likeBook(bookId) {
+    return await api.post(host + `/data/likes`, bookId);
 }
 
-//Create comment (Comments)
-export async function createComment(comment){
-    return await api.post(host + `/data/comments`, comment);
+//getTotaolLikesCount
+export async function getTotalLikesCount(bookId){
+    return await api.get(host + `/data/likes?where=bookId%3D%22${bookId}%22&distinct=_ownerId&count`);
 }
 
-//likePet (Likes)
-export async function likePet(petId) {
-    return await api.post(host + `/data/likes`, petId);
-}
-
-//getTotaolLikesCount (Likes)
-export async function getTotalLikesCount(petId){
-    return await api.get(host + `/data/likes?where=petId%3D%22${petId}%22&distinct=_ownerId&count`);
-}
-
-//did user like pet (Likes) returns 0 if not liked OR 1 if liked
-export async function didUserLikePet(petId, userId){
-    return await api.get(host + `/data/likes?where=petId%3D%22${petId}%22%20and%20_ownerId%3D%22${userId}%22&count`);
-}
-
-// Search
-export async function search(query) {
-    return await api.get(host + `/data/wiki?where=title%20LIKE%20%22${query}%22`);
-}
-
-// Paging
-export async function getColletionSize() {
-    return await api.get(host + `/data/cars?count`);
-}
-
-// Paging
-export async function getAllListingsForPaging(page = 1) {
-    return await api.get(host + `/data/cars?sortBy=_createdOn%20desc&offset=${(page - 1) * 3}&pageSize=3`);
+//did user like returns 0 if not liked OR 1 if liked
+export async function didUserLikeBook(bookId, userId){
+    return await api.get(host + `/data/likes?where=bookId%3D%22${bookId}%22%20and%20_ownerId%3D%22${userId}%22&count`);
 }

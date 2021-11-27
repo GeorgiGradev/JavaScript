c60bd90e938419348a5487d655bebec3c65b080f
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-11-27 12:26:20 +0200

    Add details.js

--- 03. JS Applications/09. Exams/Pet_My_Pet_sandbox/src/app.js
+++ 03. JS Applications/09. Exams/Pet_My_Pet_sandbox/src/app.js
@@ -1,2 +1,43 @@
 import * as api from './api/data.js';
 window.api = api;
+
+import { render, page } from './lib.js';
+import { logout as apiLogout } from "./api/data.js";
+import { getUserData } from './utility.js';
+import { loginPage, registerPage } from './views/auth.js';
+import { homePage } from './views/home.js';
+
+const main = document.querySelector('main');
+document.getElementById('logoutBtn').addEventListener('click', logout, false);
+
+setUserNav();
+page(decorateContext);
+page('/login', loginPage);
+page('/register', registerPage);
+page('/', homePage);
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, main);
+    ctx.setUserNav = setUserNav;
+    ctx.user = getUserData();
+    next();
+}
+
+function setUserNav() {
+    const user = getUserData();
+    if (user){
+        document.getElementById('user').style.display = 'inline-block';
+        document.getElementById('guest').style.display = 'none';
+        document.querySelector('#user span').textContent = `Welcome, ${user.email}`;
+    } else {
+        document.getElementById('user').style.display = 'none';
+        document.getElementById('guest').style.display = 'inline-block';
+    }
+}
+
+async function logout() {
+    apiLogout();
+    setUserNav();
+    page.redirect('/');
+}

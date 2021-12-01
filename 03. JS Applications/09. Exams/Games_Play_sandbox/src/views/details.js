d3f3500b21c79a98784b7c0749df2e11fcb9bcd9
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-12-01 19:03:47 +0200

    Add comments functionality

--- 03. JS Applications/09. Exams/Games_Play_sandbox/src/views/details.js
+++ 03. JS Applications/09. Exams/Games_Play_sandbox/src/views/details.js
@@ -1,7 +1,8 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
-import { deleteGame, getGameById } from '../api/data.js';
+import { deleteGame, getGameById, createComment, getComments } from '../api/data.js';
+import { getUserData } from '../utility.js';
 
-const detailsTemplate = (game, isOwner, onDelete) => html`
+const detailsTemplate = (game, isOwner, onDelete, comments, onClickComment, user) => html`
 <section id="game-details">
     <h1>Game Details</h1>
     <div class="info-section">
@@ -13,25 +14,18 @@ const detailsTemplate = (game, isOwner, onDelete) => html`
             <p class="type">${game.category}</p>
         </div>
 
-        <p class="text">${game.summary}</p> </p>
+        <p class="text">${game.summary}</p>
+        </p>
 
         <!-- Bonus ( for Guests and Users ) -->
         <div class="details-comments">
             <h2>Comments:</h2>
+            ${comments.length == 0 ? html`<p class="no-comment">No comments.</p>` : html``}
             <ul>
-                <!-- list all comments for current game (If any) -->
-                <li class="comment">
-                    <p>Content: I rate this one quite highly.</p>
-                </li>
-                <li class="comment">
-                    <p>Content: The best game.</p>
-                </li>
+                ${comments.map(commentTemplate)}
             </ul>
-            <!-- Display paragraph: If there are no games in the database -->
-            <p class="no-comment">No comments.</p>
         </div>
 
-        <!-- Edit/Delete buttons ( Only for creator of this game )  -->
         ${isOwner ? html`<div class="buttons">
             <a href="/edit/${game._id}" class="button">Edit</a>
             <a @click=${onDelete} href="javascript:void(0)" class="button">Delete</a>
@@ -40,27 +34,53 @@ const detailsTemplate = (game, isOwner, onDelete) => html`
 
     <!-- Bonus -->
     <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) -->
-    <article class="create-comment">
+    ${user && !isOwner ? html`<article class="create-comment">
         <label>Add new comment:</label>
-        <form class="form">
-            <textarea name="comment" placeholder="Comment......"></textarea>
+        <form @submit=${onClickComment} class="form">
+            <textarea id="addComment" name="comment" placeholder="Comment......"></textarea>
             <input class="btn submit" type="submit" value="Add Comment">
         </form>
-    </article>
-
+    </article>` : null}
 </section>`;
 
-export async function detailsPage(ctx){
+const commentTemplate = (comment) => html`
+<li class="comment">
+    <p>Content: ${comment.comment}</p>
+</li>`;
+
+
+export async function detailsPage(ctx) {
     const gameId = ctx.params.id;
     const game = await getGameById(gameId);
-    const isOwner = ctx.user && game._ownerId == ctx.user._id;
-    ctx.render(detailsTemplate(game, isOwner, onDelete));
+    const user = getUserData();
+    const isOwner = user && game._ownerId == user._id;
+    let comments = await getComments(gameId);
+    update();
 
-    async function onDelete(){
+    async function onDelete() {
         const confirmed = confirm('Are you sure?');
-        if (confirmed){
+        if (confirmed) {
             await deleteGame(gameId);
             ctx.page.redirect('/');
         }
+    }
+
+    async function onClickComment(event) {
+        event.preventDefault();
+        const textArea = document.getElementById("addComment");
+        if (textArea.value != "") {
+            await createComment({
+                gameId: gameId,
+                comment: textArea.value
+            });
+            comments = await getComments(gameId);
+            event.target.reset();
+            update();
+        } else {
+            return alert("Please enter comment!");
+        }
+    }
+    async function update() {
+        ctx.render(detailsTemplate(game, isOwner, onDelete, comments, onClickComment, user));
     }
 }

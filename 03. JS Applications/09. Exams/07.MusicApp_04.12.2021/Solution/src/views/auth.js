13cd52ce274b45216d816453c912617b106aa5b7
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-12-05 16:56:09 +0200

    Create auth.js

---
+++ 03. JS Applications/09. Exams/07.MusicApp_04.12.2021/Solution/src/views/auth.js
@@ -0,0 +1,88 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { login, register } from '../api/data.js';
+                    
+const loginTemplate = (onSubmit) => html`
+<section id="loginPage">
+    <form @submit=${onSubmit}>
+        <fieldset>
+            <legend>Login</legend>
+
+            <label for="email" class="vhide">Email</label>
+            <input id="email" class="email" name="email" type="text" placeholder="Email">
+
+            <label for="password" class="vhide">Password</label>
+            <input id="password" class="password" name="password" type="password" placeholder="Password">
+
+            <button type="submit" class="login">Login</button>
+
+            <p class="field">
+                <span>If you don't have profile click <a href="/register">here</a></span>
+            </p>
+        </fieldset>
+    </form>
+</section>`;
+
+export async function loginPage(ctx) {
+    ctx.render(loginTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+
+        await login(email, password);
+        
+        event.target.reset();
+        ctx.setUserNav();
+        ctx.page.redirect('/');
+    }
+}
+                      
+const registerTemplate = (onSubmit) => html`
+<section id="registerPage">
+    <form @submit=${onSubmit}>
+        <fieldset>
+            <legend>Register</legend>
+
+            <label for="email" class="vhide">Email</label>
+            <input id="email" class="email" name="email" type="text" placeholder="Email">
+
+            <label for="password" class="vhide">Password</label>
+            <input id="password" class="password" name="password" type="password" placeholder="Password">
+
+            <label for="conf-pass" class="vhide">Confirm Password:</label>
+            <input id="conf-pass" class="conf-pass" name="conf-pass" type="password" placeholder="Confirm Password">
+
+            <button type="submit" class="register">Register</button>
+
+            <p class="field">
+                <span>If you already have profile click <a href="/login">here</a></span>
+            </p>
+        </fieldset>
+    </form>
+</section>`;
+
+export async function registerPage(ctx) {
+    ctx.render(registerTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+        const repeatPass = formData.get('conf-pass').trim();
+
+        if (!email || !password || !repeatPass) {
+            return alert('All fields are requirerd!');
+        }
+        if (password != repeatPass) {
+            return alert('Password don\'t match!');
+        }
+        await register(email, password);
+        
+        event.target.reset();
+        ctx.setUserNav();
+        ctx.page.redirect('/');
+    }
+}

50e88e38458093c2a64d0434abb49a8f49f6869b
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-12-16 22:05:11 +0200

    Remove not need files

--- 03. JS Applications/09. Exams/Online_Books_Library_sandbox/src/views/common/book.js
+++
@@ -1,9 +0,0 @@
-import { html } from '../../../node_modules/lit-html/lit-html.js';
-
-export const bookTemplate = (book) => html`
-<li class="otherBooks">
-    <h3>${book.title}</h3>
-    <p>Type: ${book.type}</p>
-    <p class="img"><img src=${book.imageUrl}></p>
-    <a class="button" href="/details/${book._id}">Details</a>
-</li>`;
--- 03. JS Applications/09. Exams/SoftWiki_sandbox/src/views/common/article.js
+++
@@ -1,4 +0,0 @@
-import { html } from '../../../node_modules/lit-html/lit-html.js';
-
-export const articleTemplate = (article) => html`
-`;

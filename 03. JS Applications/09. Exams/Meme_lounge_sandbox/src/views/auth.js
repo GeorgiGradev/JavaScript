0817c164c9b954e578b3276107fc728ea6c1855c
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-12-28 10:07:53 +0200

    Create auth.js

---
+++ 03. JS Applications/09. Exams/Meme_lounge_sandbox/src/views/auth.js
@@ -0,0 +1,99 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { login, register } from '../api/data.js';
+
+const loginTemplate = (onSubmit) => html`
+<section id="login">
+    <form @submit=${onSubmit} id="login-form">
+        <div class="container">
+            <h1>Login</h1>
+            <label for="email">Email</label>
+            <input id="email" placeholder="Enter Email" name="email" type="text">
+            <label for="password">Password</label>
+            <input id="password" type="password" placeholder="Enter Password" name="password">
+            <input type="submit" class="registerbtn button" value="Login">
+            <div class="container signin">
+                <p>Dont have an account?<a href="/register">Sign up</a>.</p>
+            </div>
+        </div>
+    </form>
+</section>`;
+
+export async function loginPage(ctx) {
+    ctx.render(loginTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+
+        try{
+            if (!email || !password) {
+                throw new Error("All fields are required!");
+            }
+            await login(email, password);
+            event.target.reset();
+            ctx.setUserNav();
+            ctx.page.redirect('/catalog');
+        } catch(err) {
+            notify(err.message);
+        }
+    }
+}
+
+const registerTemplate = (onSubmit) => html`
+<section id="register">
+    <form @submit=${onSubmit} id="register-form">
+        <div class="container">
+            <h1>Register</h1>
+            <label for="username">Username</label>
+            <input id="username" type="text" placeholder="Enter Username" name="username">
+            <label for="email">Email</label>
+            <input id="email" type="text" placeholder="Enter Email" name="email">
+            <label for="password">Password</label>
+            <input id="password" type="password" placeholder="Enter Password" name="password">
+            <label for="repeatPass">Repeat Password</label>
+            <input id="repeatPass" type="password" placeholder="Repeat Password" name="repeatPass">
+            <div class="gender">
+                <input type="radio" name="gender" id="female" value="female">
+                <label for="female">Female</label>
+                <input type="radio" name="gender" id="male" value="male" checked>
+                <label for="male">Male</label>
+            </div>
+            <input type="submit" class="registerbtn button" value="Register">
+            <div class="container signin">
+                <p>Already have an account?<a href="/login">Sign in</a>.</p>
+            </div>
+        </div>
+    </form>
+</section>`;
+
+export async function registerPage(ctx) {
+    ctx.render(registerTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const username = formData.get('username').trim();
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+        const repeatPass = formData.get('repeatPass').trim();
+        const gender = formData.get('gender');
+        
+        try{
+            if (!username, !email || !password || !repeatPass) {
+                throw new Error('All fields are requirerd!');
+            }
+            if (password != repeatPass) {
+                throw new Error('Password don\'t match!');
+            }
+            await register(username, email, password, gender);
+            event.target.reset();
+            ctx.setUserNav();
+            ctx.page.redirect('/catalog');
+        } catch(err) {
+            notify(err.message);
+        }
+
+    }
+}

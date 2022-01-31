d35478e71bafbe5b0f3c8fe02761e3273035456b
Georgi Gradev <georgi.d.gradev@gmail.com>
2022-01-31 18:25:46 +0200

    Create catalog.js

---
+++ 03. JS Applications/09. Exams/SoftWiki_sandbox/src/views/catalog.js
@@ -0,0 +1,21 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { getAllArticles } from '../api/data.js';
+
+const catalogTemplate = (articles) => html`
+<section id="catalog-page" class="content catalogue">
+    <h1>All Articles</h1>
+    ${articles.length == 0 ? html`<h3 class="no-articles">No articles yet</h3>` : articles.map(articleTemplate)}
+</section>`;
+
+const articleTemplate = (article) => html`
+    <a class="article-preview" href="/details/${article._id}">
+        <article>
+            <h3>Topic: <span>${article.title}</span></h3>
+            <p>Category: <span>${article.category}</span></p>
+        </article>
+    </a>`;
+
+export async function catalogPage(ctx) {
+    const articles = await getAllArticles();
+    ctx.render(catalogTemplate(articles));
+}
